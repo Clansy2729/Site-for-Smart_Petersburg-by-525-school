0fea3699ef4a2aa919031b04245d474db5153b16
Konstantin Alpatov <kraken2000abvgd@mail.ru>
2024-01-15 18:42:27 +0300

    jjjj

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //коммент для появления файла на сайте
+function sendEmail(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+   
+   Window.open("mailto:kraken2000abvgd@gmail.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body));
+    
+    
+}
+
+
