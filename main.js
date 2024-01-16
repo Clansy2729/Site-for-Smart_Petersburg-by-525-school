ebdd7a802699f9421ad2b8e73a86817b8824338e
Konstantin Alpatov <kraken2000abvgd@mail.ru>
2024-01-16 18:13:59 +0300

    рп

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
 //коммент для появления файла на сайте
-function sendEmail(){
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-   
-   window.open("http://ya.ru");
-    
-    
-}
 
 
